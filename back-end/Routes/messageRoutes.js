dd7b1977ee64b3a3415eb3c5bd24882d7c85c48b
kaisboubakri <kais.boubakri1998@gmail.com>
2024-02-25 17:30:46 +0100

    updated

--- back-end/Routes/messageRoutes.js
+++ back-end/Routes/messageRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   allMessages,
   sendMessage,
-} = require("../Controllers/messageControllers");
+} = require("../controllers/messageControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
